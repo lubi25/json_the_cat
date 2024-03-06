44e3bfcc6a9ec6d99851e5e460f01688b0d5e84d
Lubi Islam <lubaba.islam91@gmail.com>
2024-03-05 21:45:59 -0500

    Implement fetchBreedDescription function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,18 @@
 const request = require('request');
 
+function fetchBreedDescription(breed) {
+
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+
+  request(url, function(_, _, body) {
+
+    const data = JSON.parse(body);
+
+    const bodyText = data[0];
+
+    console.log(bodyText.description);
+  });
+}
+
 const breed = process.argv[2];
-
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
-
-request(url, function(_, _, body) {
-
-  const data = JSON.parse(body);
-
-  const bodyText = data[0];
-
-  console.log(bodyText.description);
-});
+fetchBreedDescription(breed);
