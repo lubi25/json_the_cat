ae93de12deeca196623a3cb025286e8243107788
Lubi Islam <lubaba.islam91@gmail.com>
2024-03-05 21:58:33 -0500

    Add mocha and chai test

---
+++ test/breedFetcherTest.js
@@ -0,0 +1,32 @@
+const { fetchBreedDescription } = require('../breedFetcher');
+const { assert } = require('chai');
+
+describe('fetchBreedDescription', () => {
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      // we expect no error for this scenario
+      assert.equal(err, null);
+
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+
+      // compare returned description
+      assert.equal(expectedDesc, desc.trim());
+
+      done();
+    });
+  });
+
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Maine Coon', (err, desc) => {
+      // we expect no error for this scenario
+      assert.equal(err, null);
+
+      const expectedDesc = "They are known for their size and luxurious long coat Maine Coons are considered a gentle giant. The good-natured and affable Maine Coon adapts well to many lifestyles and personalities. She likes being with people and has the habit of following them around, but isn’t needy. Most Maine Coons love water and they can be quite good swimmers.";
+
+      // compare returned description
+      assert.equal(expectedDesc, desc.trim());
+
+      done();
+    });
+  });
+});
